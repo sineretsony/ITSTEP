7f1deb33f4f331480d500de2804ee28013e8bafe
Gorea Gregoire <sineretsony@gmail.com>
2023-10-07 19:51:51 +0300

    uncommenting

--- 012.py
+++ 012.py
@@ -1,60 +1,60 @@
-# print("test")
-#
-# name = "McGregor"
-# number = 10
-# number1 = 10.2
-# print(type(number))
-# print(type(number1))
-#
-# print(4 / 2.0)
-#
-# number3 = input("Enter num: ")
-# print("number", int(number3)*2)
+print("test")
 
-# num = 3
-# print(f"num {num}")
-#
-# a = 10
-# b = 2
-#
-# sum_result = a + b
-# difference_result = a - b
-# product_result = a * b
-# quotient_result = a / b
-#
-# print("Сумма:", sum_result)
-# print("Разность:", difference_result)
-# print("Произведение:", product_result)
-# print("Частное:", quotient_result)
-#
-# c = 15
-# d = 2
-#
-# result = c + d
-# print(f"Результат: {result}")
-#
-#
-# distance = float(input("Введите расстояние до аэропорта (в км): "))
-# time = float(input("Введите время в пути (в часах): "))
-#
-# speed = distance / time
-# print(f"Ехать нужно со скоростью: {speed} км/ч.")
+name = "McGregor"
+number = 10
+number1 = 10.2
+print(type(number))
+print(type(number1))
 
-# start_h = int(input("Введите час начала: "))
-# start_min = int(input("Введите минуту начала: "))
-# start_sec = int(input("Введите секунду начала: "))
-#
-# end_h = int(input("Введите час завершения: "))
-# end_min = int(input("Введите минуту завершения разговора: "))
-# end_sec = int(input("Введите секунду завершения разговора: "))
-#
-# start_seconds = start_h * 3600 + start_min * 60 + start_sec
-# endSeconds = end_h * 3600 + end_min * 60 + end_sec
-# seconds = endSeconds - start_seconds
-# cos = 0.30
-# cop = (seconds / 60) * cos
-#
-# print("Стоимость разговора:", cop, "грн.")
+print(4 / 2.0)
+
+number3 = input("Enter num: ")
+print("number", int(number3)*2)
+
+num = 3
+print(f"num {num}")
+
+a = 10
+b = 2
+
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+quotient_result = a / b
+
+print("Сумма:", sum_result)
+print("Разность:", difference_result)
+print("Произведение:", product_result)
+print("Частное:", quotient_result)
+
+c = 15
+d = 2
+
+result = c + d
+print(f"Результат: {result}")
+
+
+distance = float(input("Введите расстояние до аэропорта (в км): "))
+time = float(input("Введите время в пути (в часах): "))
+
+speed = distance / time
+print(f"Ехать нужно со скоростью: {speed} км/ч.")
+
+start_h = int(input("Введите час начала: "))
+start_min = int(input("Введите минуту начала: "))
+start_sec = int(input("Введите секунду начала: "))
+
+end_h = int(input("Введите час завершения: "))
+end_min = int(input("Введите минуту завершения разговора: "))
+end_sec = int(input("Введите секунду завершения разговора: "))
+
+start_seconds = start_h * 3600 + start_min * 60 + start_sec
+endSeconds = end_h * 3600 + end_min * 60 + end_sec
+seconds = endSeconds - start_seconds
+cos = 0.30
+cop = (seconds / 60) * cos
+
+print("Стоимость разговора:", cop, "грн.")
 
 
 dist = float(input("Расстояние поездки км: "))
